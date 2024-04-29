cb80a4b8354fbaa4482e1d8de21802f81861f0af
Aniruddh Iyer <skyp_meinrath@simplelogin.com>
2024-04-29 20:05:03 +0530

    prediction function taking in preprocced text

--- python/analysis/sentiment.py
+++ python/analysis/sentiment.py
@@ -1,11 +1,6 @@
 sentiment_model = load_model()
 
 def predict_with_ensemble_model(text):
-    preprocessed_text,tf-idf_vector = preprocess_text_with_tfidf(text)
+    _,tf-idf_vector = preprocess_text_with_tfidf(text)
     predictions = sentiment_model.predict(tfidf_vector)
-    result = {
-        "preprocessed_text": preprocessed_text,
-        "sentiment_score": sentiment_score
-    }
-    
-    return result
+    return predictions
