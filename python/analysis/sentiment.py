74acbd71106bbc3f3e3709bbaf626dec2163df94
Aniruddh Iyer <skyp_meinrath@simplelogin.com>
2024-04-29 15:36:04 +0530

    feed in text and return preprocessed text and sentiment

--- python/analysis/sentiment.py
+++ python/analysis/sentiment.py
@@ -1,5 +1,11 @@
 sentiment_model = load_model()
 
-def predict_with_ensemble_model(tfidf_vector):
+def predict_with_ensemble_model(text):
+    preprocessed_text,tf-idf_vector = preprocess_text_with_tfidf(text)
     predictions = sentiment_model.predict(tfidf_vector)
-    return sentiment_score
+    result = {
+        "preprocessed_text": preprocessed_text,
+        "sentiment_score": sentiment_score
+    }
+    
+    return result
