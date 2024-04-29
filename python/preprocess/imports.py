33aebdec1a257347227fcc4718b1f9ad22e690d7
Aniruddh Iyer <skyp_meinrath@simplelogin.com>
2024-04-29 09:31:46 +0530

    import tfidf vectorizer

--- python/preprocess/imports.py
+++ python/preprocess/imports.py
@@ -4,14 +4,10 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import re
-import tensorflow as tf
-import tensorflow_hub as hub
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
-
-# Download Universal Sentence Encoder module
-module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/5"
-embed = hub.load(module_url) 
 
