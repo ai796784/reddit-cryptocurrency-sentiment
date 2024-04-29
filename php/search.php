c59a73e00ba503b66fa2d36f382856516b108706
Aniruddh Iyer <skyp_meinrath@simplelogin.com>
2024-04-29 20:02:19 +0530

    created variables to fetch data

--- php/search.php
+++ php/search.php
@@ -7,11 +7,20 @@ $subreddit = $_POST['subreddit'];
 
 // Call the Python script with PRAW to fetch data from Reddit
 // Example:
-$output = shell_exec("python ../python/fetch/reddit_search.py $subreddit_name");
-$table = 'posts';
-insertDataIntoDB($table, $output);
+$posts = shell_exec("python ../python/fetch/reddit_search.py '$subreddit_name'");
+$postArray = explode("\n", $posts);
+$interactions = shell_exec("python ../python/fetch/author_interaction '$posts'");
+
+foreach ($postArray as $post) {
+    // Accessing the "selftext" key of each post
+    $body = $post['selftext'];
+    $text_vector = shell_exec("python ../python/fetch/reddit_search.py '$body'");
+    $values = explode(" ", $text_vector);
+    $body_sentiment = $values[0];
+    $body_vector = $values[1];
+    $body_emotion = shell_exec("python ../python/fetch/reddit_search.py '$body'");
+}
 
 // Redirect back to the homepage or any other page after processing
 header("Location: index.html");
 exit();
-?>
