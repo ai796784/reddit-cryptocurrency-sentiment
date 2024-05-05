63594b0d13eeaeb8df004eb5d70bf97d31f302bc
Shashwat-Tandon <shashwat.tandon2020@vitstudent.ac.in>
2024-05-05 18:47:00 +0530

    Modified the frontend. Check script.js for functioning.

--- static/script.js
+++ static/script.js
@@ -1,74 +1,80 @@
-document.getElementById("redditForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent default form submission behavior
-  
-    var subredditName = document.getElementById("subreddit").value;
-  
-    // Make AJAX request to fetch data from PHP script
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "../php/search.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onerror = function() {
-      console.error("An error occurred while making the AJAX request.");
-    };
-  
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          // Parse the JSON response
-          console.log(xhr.responseText)
-          
-          var responseData = JSON.parse(xhr.responseText);
-  
-          // Call functions to create charts with the received data
-          // createLineChart(responseData.linePlotData);
-          //createBarChart(responseData.barPlotData);
-          // createPieChart(responseData.piePlotData);
-          //createHeatMap(responseData.heatPlotData);
-          // createDonutChart(responseData.piePloData);
-          // createRadarChart(responseData.radarPlotData);
-          //createNetworkGraph(responseData.networkPlotData);
+var plotPaths = {};
 
-          // Define an object to map plot types to their paths
-          var newDirectory = "../reddit_data/plots/";
+document.getElementById("redditForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent default form submission behavior
 
-          var plotPaths = {
-            "line": responseData.linePlotResponse,
-            "pie": responseData.piePlotResponse,
-            "donut": responseData.donutPlotResponse,
-            "heat": responseData.heatPlotResponse,
-            "radar": responseData.radarPlotResponse,
-            "network": responseData.networkPlotResponse
-          };
-          
-          for (var plotType in plotPaths) {
-            if (plotPaths.hasOwnProperty(plotType)) {
-              var plotPath = plotPaths[plotType];
-              var newPath = newDirectory + plotPath
+  var subredditName = document.getElementById("subreddit").value;
 
-              var container = document.getElementById(plotType + "ChartCanvas");
-              
-              // Create a new image element
-              var img = document.createElement('img');
-              
-              // Set the source of the image
-              img.src = newPath;
-              
-              // Append the image to the container div
-              container.appendChild(img);
-            
+  // Make AJAX request to fetch data from PHP script
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "../../php/search.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.onerror = function () {
+    console.error("An error occurred while making the AJAX request.");
+  };
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        // Parse the JSON response
+        console.log(xhr.responseText)
+
+        var responseData = JSON.parse(xhr.responseText);
+
+        // Call functions to create charts with the received data
+        // createLineChart(responseData.linePlotData);
+        //createBarChart(responseData.barPlotData);
+        // createPieChart(responseData.piePlotData);
+        //createHeatMap(responseData.heatPlotData);
+        // createDonutChart(responseData.piePloData);
+        // createRadarChart(responseData.radarPlotData);
+        //createNetworkGraph(responseData.networkPlotData);
+
+        // Define an object to map plot types to their paths
+        var newDirectory = "../reddit_data/plots/";
+
+        var plotPaths = {
+          "line": responseData.linePlotResponse,
+          "pie": responseData.piePlotResponse,
+          "donut": responseData.donutPlotResponse,
+          "heat": responseData.heatPlotResponse,
+          "radar": responseData.radarPlotResponse,
+          "network": responseData.networkPlotResponse
+        };
+
+        for (var plotType in plotPaths) {
+          if (plotPaths.hasOwnProperty(plotType)) {
+            var plotPath = plotPaths[plotType];
+            var newPath = newDirectory + plotPath
+
+            var container = document.getElementById(plotType + "ChartCanvas");
+            // Remove existing image elements
+            while (container.firstChild) {
+              container.removeChild(container.firstChild);
             }
 
+            // Create a new image element
+            var img = document.createElement('img');
+
+            // Set the source of the image
+            img.src = newPath;
+
+            // Append the image to the container div
+            container.appendChild(img);
+
           }
-        } else {
-          console.error("AJAX request failed");
+
         }
+      } else {
+        console.error("AJAX request failed");
       }
-    };
-  
-    // Encode data to be sent in the request body
-    var params = "subredditName=" + encodeURIComponent(subredditName);
-    xhr.send(params);
-  });
+    }
+  };
+
+  // Encode data to be sent in the request body
+  var params = "subredditName=" + encodeURIComponent(subredditName);
+  xhr.send(params);
+});
 
 
 //   document.getElementById("redditForm").addEventListener("reset", function(event) {
@@ -78,3 +84,16 @@ document.getElementById("redditForm").addEventListener("submit", function(event)
 //         container.innerHTML = ""; // Clear the container
 //     });
 // });
+
+// Add event listener for the "Clear" button
+document.getElementById("redditForm").addEventListener("reset", function (event) {
+  var plotTypes = ["line", "pie", "donut", "heat", "radar", "network"];
+  for (plotType in plotPaths) {
+    if (plotPaths.hasOwnProperty(plotType)) {
+      var container = document.getElementById(plotType + "ChartCanvas");
+      while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    }
+  }
+});
